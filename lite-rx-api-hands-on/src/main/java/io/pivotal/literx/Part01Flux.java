50c8333a4ecb622c8ed95a3aa5a258dfbdf5e3fe
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2022-05-26 23:55:46 -0500

    Primer punto

---
+++ lite-rx-api-hands-on/src/main/java/io/pivotal/literx/Part01Flux.java
@@ -0,0 +1,59 @@
+package io.pivotal.literx;
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Learn how to create Flux instances.
+ *
+ * @author Sebastien Deleuze
+ * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html">Flux Javadoc</a>
+ */
+public class Part01Flux {
+
+//========================================================================================
+
+	// TODO Return an empty Flux
+	Flux<String> emptyFlux() {
+		return Flux.empty();
+	}
+
+//========================================================================================
+
+	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+
+
+	Flux<String> fooBarFluxFromValues() {
+		return Flux.just("foo", "bar");
+	}
+
+//========================================================================================
+
+	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+	Flux<String> fooBarFluxFromList() {
+		ArrayList<String> LISTA = new ArrayList<String>();
+		// Añade el elemento al ArrayList
+		LISTA.add("foo");
+		LISTA.add("bar");
+
+		return Flux.just(LISTA.get(0),LISTA.get(1));
+	}
+
+//========================================================================================
+
+	// TODO Create a Flux that emits an IllegalStateException
+	Flux<String> errorFlux() {
+		return Flux.error(new IllegalStateException("Error"));
+	}
+
+//========================================================================================
+
+		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+	Flux<Long> counter() {
+		return Flux.interval(Duration.ofMillis(100)).take(9);
+	}
+
+}
